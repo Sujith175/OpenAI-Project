3001b68c937a81dc9228aee6eb073f7256dea5cb
Sujith Kumar R <sujithkumar.sk175@gmail.com>
2023-01-31 01:01:38 +0530

    feat: front end completed

---
+++ client/src/components/ChatBody.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const ChatBody = ({ chat }) => {
+  const aiStyle =
+    "bg-white bg-opacity-40 backdrop-blur-lg dropshadow-md mr-auto";
+  return (
+    <div className="flex flex-col gap-4 ">
+      {chat.map((message, i) => {
+        return (
+          <div
+            key={i}
+            className={`border-[#999999] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%] ${
+              message.sender === "ai" && aiStyle
+            }`}
+          >
+            <pre className="whitespaces-pre-wrap">
+              <span>{message.message}</span>
+            </pre>
+          </div>
+        );
+      })}
+
+      {/* AI Message */}
+      <div
+        className={`border-[#999999] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%]'
+        <pre className= 'whitespaces-pre-wrap ${aiStyle}`}
+      >
+        <pre>
+          <span>Yeah, I can Help you with anything</span>
+        </pre>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBody;
---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import env from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+const app = express();
+
+env.config();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//configure open API
+const configuration = new Configuration({
+  organization: "org-kWW5SqStgfRcTsvCSrnCYYYa",
+  apiKey: process.env.API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+//Listening
+app.listen("3080", () => console.log("Listening on Port 3080"));
+
+//post Route making request
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  try {
+    const response = await openai.createCompletion({
+      model: " text-davinci-003",
+      prompt: `${message}`,
+      max_tokens: 100,
+      temperature: 0.5,
+    });
+    res.json({ message: response.data.choices[0].text });
+  } catch (error) {
+    console.log(error);
+    res.send(error).status(400);
+  }
+});
